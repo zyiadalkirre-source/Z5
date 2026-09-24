b2620987cc149e95db3a1d4d111b06ba257d6a9b
zyiadalkirre-source <zyiadalkirre@gmail.com>
2026-09-25 02:44:46 +0300

    fix(lint): configure flat ESLint for Astro

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from "@eslint/js";
+import astro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", ".astro/**", ".z5-cache/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+);
